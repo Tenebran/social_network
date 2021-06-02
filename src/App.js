e31fb0f6c404da1125f1b35968bdb001be0ed1c9
Sergej Garkusha <Tenebrann@googlemail.com>
2021-06-02 23:29:10 +0200

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './modules/components/Header/Header';
+import Profile from './modules/components/Profile/Profile';
+import Siderbar from './modules/components/Sidebar/Siderbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="social__wrapper">
+      <Header />
+      <div className="content__conteiner content__conteiner_menu">
+        <Siderbar />
+        <Profile />
+      </div>
     </div>
   );
 }
---
+++ src/modules/components/Profile/Profile.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Profile.scss';
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <div className="profile__right">
+        <div className="profile__photo">
+          <img
+            className="profile__img"
+            src="https://sun9-72.userapi.com/impf/c840431/v840431829/430f3/3aV51o5M5ng.jpg?size=640x428&quality=96&sign=7811429587d08016511bee6a72bb1f1b&type=album"
+          ></img>
+        </div>
+      </div>
+
+      <div className="profile__left">
+        <div className="profile__person">
+          <div className="profile__person_wrapp">
+            <span className="profile__person_name">Iruska</span>
+            <span className="profile__person_surname">Stepanyuk</span>
+          </div>
+          <div className="profile__label">
+            <span className="profile__label-name"> Birthday:</span>
+            <span className="profile__label-more">September 12, 2000</span>
+          </div>
+          <div className="profile__label">
+            <span className="profile__label-name"> Current city:</span>
+            <span className="profile__label-more">Odessa</span>
+          </div>
+          <div className="profile__friends">
+            <img
+              className="profile__friends_photo"
+              src="https://sun9-20.userapi.com/impg/nQo9r7AS1nSeEhiaf3Nt7NrMCUI2rwON_WIE0Q/nq065pRrfmY.jpg?size=1344x2160&quality=95&sign=f8ddbf76f5d328636f0eab77cbcefc68&type=album"
+            ></img>
+
+            <img
+              className="profile__friends_photo"
+              src="https://sun9-20.userapi.com/impg/nQo9r7AS1nSeEhiaf3Nt7NrMCUI2rwON_WIE0Q/nq065pRrfmY.jpg?size=1344x2160&quality=95&sign=f8ddbf76f5d328636f0eab77cbcefc68&type=album"
+            ></img>
+            <img
+              className="profile__friends_photo"
+              src="https://sun9-20.userapi.com/impg/nQo9r7AS1nSeEhiaf3Nt7NrMCUI2rwON_WIE0Q/nq065pRrfmY.jpg?size=1344x2160&quality=95&sign=f8ddbf76f5d328636f0eab77cbcefc68&type=album"
+            ></img>
+            <img
+              className="profile__friends_photo"
+              src="https://sun9-20.userapi.com/impg/nQo9r7AS1nSeEhiaf3Nt7NrMCUI2rwON_WIE0Q/nq065pRrfmY.jpg?size=1344x2160&quality=95&sign=f8ddbf76f5d328636f0eab77cbcefc68&type=album"
+            ></img>
+
+            <img
+              className="profile__friends_photo"
+              src="https://sun9-20.userapi.com/impg/nQo9r7AS1nSeEhiaf3Nt7NrMCUI2rwON_WIE0Q/nq065pRrfmY.jpg?size=1344x2160&quality=95&sign=f8ddbf76f5d328636f0eab77cbcefc68&type=album"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+{
+  /* <img
+          className="social__content_person-wrapper"
+          src="https://cdnb.artstation.com/p/users/covers/000/657/093/default/45917376ed35169f5c6720ed99c86761.jpg?1574479568"
+        ></img> */
+}
